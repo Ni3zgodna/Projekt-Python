d2ffbed54e3133fb2c7c941a1d4cedb0b0456381
Zuzanna <zbialas123@gmail.com>
2024-06-03 12:21:44 +0200

    Dodałam ruch gracza.

--- gracz.py
+++ gracz.py
@@ -9,6 +9,14 @@ class Gracz:
         self.wyglad.fill((0, 0, 0))
         self.kwadrat = self.wyglad.get_rect(center=(self.szerokosc, self.wysokosc - 70))
         # zastąpić kwadrat wczytaniem obrazu
+        
+        self.predkosc = 10
+
+    def move(self, klawisze):
+        if klawisze[pygame.K_LEFT] and self.kwadrat.left > 0:
+            self.kwadrat.x -= self.predkosc
+        if klawisze[pygame.K_RIGHT] and self.kwadrat.right < self.szerokosc:
+            self.kwadrat.x += self.predkosc
 
     def draw(self, powierzchnia):
         powierzchnia.blit(self.wyglad, self.kwadrat)
